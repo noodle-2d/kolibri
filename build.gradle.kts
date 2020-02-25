d8a529c89ca4744ded218d6f6df847472b9b7aec
Ilya Kuznetsov <et.anchoret@gmail.com>
2020-02-25 23:19:41 +0300

    Parent build.gradle.kts was supplemented by Spring and Kotlin dependencies and plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,21 @@
+import org.jetbrains.kotlin.allopen.gradle.AllOpenExtension
+
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.50" apply false
+    kotlin("jvm") version "1.3.61" apply false
+    kotlin("plugin.allopen") version "1.3.61" apply false
+
+    id("org.springframework.boot") version "2.0.5.RELEASE" apply false
+    id("io.spring.dependency-management") version "1.0.7.RELEASE" apply false
 }
 
 subprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "org.jetbrains.kotlin.plugin.allopen")
     apply(plugin = "idea")
 
     if (name == "kolibri-commandline-utility" || name == "kolibri-telegram-bot") {
+        apply(plugin = "org.springframework.boot")
         apply(plugin = "application")
     } else {
         apply(plugin = "java-library")
@@ -14,7 +23,8 @@ subprojects {
 
     version = "1.0"
 
-    val kotlinVersion = "1.3.50"
+    val kotlinVersion = "1.3.61"
+    val springBootVersion = "2.0.5.RELEASE"
 
     repositories {
         jcenter()
@@ -22,8 +32,17 @@ subprojects {
 
     dependencies {
         "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
+        "implementation"("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+        "implementation"("org.springframework.boot:spring-boot-dependencies:$springBootVersion")
+        "implementation"("org.springframework.boot:spring-boot-starter-web")
 
         "testImplementation"("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
         "testImplementation"("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+        "testImplementation"("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    extensions.configure<AllOpenExtension>("allOpen") {
+        annotation("org.springframework.context.annotation.Configuration")
+        annotation("org.springframework.stereotype.Component")
     }
 }
