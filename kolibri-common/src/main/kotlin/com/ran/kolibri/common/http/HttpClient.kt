8b57baf9e4a3dc083c6e7cc71457f7e0ce4c92c7
Ilya Kuznetsov <et.anchoret@gmail.com>
2020-03-01 17:37:00 +0300

    Additional debug logs were added to HttpClient.

--- kolibri-common/src/main/kotlin/com/ran/kolibri/common/http/HttpClient.kt
+++ kolibri-common/src/main/kotlin/com/ran/kolibri/common/http/HttpClient.kt
@@ -4,15 +4,21 @@ import com.ran.kolibri.common.util.logDebug
 import com.ran.kolibri.common.util.logError
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.client.ClientHttpResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.client.ResponseErrorHandler
 import org.springframework.web.client.RestTemplate
 import java.lang.RuntimeException
+import java.lang.StringBuilder
+import java.util.*
+
 
 @Component
 class HttpClient {
 
-    private val rest: RestTemplate = RestTemplate()
+    private val rest: RestTemplate = buildRestTemplate()
 
     fun <RQ, RS> post(url: String, body: RQ, responseClass: Class<RS>): RS {
         logDebug { "Send message url: $url" }
@@ -35,6 +41,26 @@ class HttpClient {
                             ?: throw HttpClientException("Unexpected status code $statusCodeValue")
                 }
                 .body ?: throw HttpClientException("Empty response body")
+
+    private fun buildRestTemplate(): RestTemplate {
+        val template = RestTemplate()
+        template.errorHandler = object : ResponseErrorHandler {
+            override fun hasError(response: ClientHttpResponse): Boolean =
+                response.statusCode == HttpStatus.BAD_REQUEST
+
+            override fun handleError(response: ClientHttpResponse) {
+                logDebug { "Response status: ${response.rawStatusCode}, ${response.statusCode}, ${response.statusText}" }
+                logDebug { "Headers: ${response.headers}" }
+                val sc = Scanner(response.body)
+                val sb = StringBuilder()
+                while (sc.hasNext()) {
+                    sb.append(sc.nextLine())
+                }
+                logDebug { "Body: $sb" }
+            }
+        }
+        return template
+    }
 }
 
 inline fun <RQ, reified RS> HttpClient.post(url: String, body: RQ): RS =
