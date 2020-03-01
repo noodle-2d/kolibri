3ed12eec1dea58849ec229a22b18f6c99a86cb37
Ilya Kuznetsov <et.anchoret@gmail.com>
2020-03-01 17:20:04 +0300

    HttpClient was supplemented by debug logs.

--- kolibri-common/src/main/kotlin/com/ran/kolibri/common/http/HttpClient.kt
+++ kolibri-common/src/main/kotlin/com/ran/kolibri/common/http/HttpClient.kt
@@ -1,10 +1,13 @@
 package com.ran.kolibri.common.http
 
+import com.ran.kolibri.common.util.logDebug
+import com.ran.kolibri.common.util.logError
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
+import java.lang.RuntimeException
 
 @Component
 class HttpClient {
@@ -12,9 +15,17 @@ class HttpClient {
     private val rest: RestTemplate = RestTemplate()
 
     fun <RQ, RS> post(url: String, body: RQ, responseClass: Class<RS>): RS {
+        logDebug { "Send message url: $url" }
         val requestEntity = HttpEntity(body)
-        val responseEntity = rest.exchange(url, HttpMethod.POST, requestEntity, responseClass)
-        return retrieveResponse(responseEntity)
+        logDebug { "Request entity: $requestEntity" }
+        try {
+            val responseEntity = rest.exchange(url, HttpMethod.POST, requestEntity, responseClass)
+            logDebug { "Response entity: $responseEntity" }
+            return retrieveResponse(responseEntity)
+        } catch (e: Throwable) {
+            logError(e) { "Error sending message" }
+            throw RuntimeException("Something went wrong")
+        }
     }
 
     private fun <RS> retrieveResponse(responseEntity: ResponseEntity<RS>): RS =
