96bee3a279236e71d9fe30b2f4dd9d568686aac4
Ilya Kuznetsov <et.anchoret@gmail.com>
2020-03-31 16:25:40 +0300

    TelegramBotStarter was implemented.

--- kolibri-telegram-bot/src/main/kotlin/com/ran/kolibri/telegram/bot/Main.kt
+++ kolibri-telegram-bot/src/main/kotlin/com/ran/kolibri/telegram/bot/Main.kt
@@ -1,8 +1,5 @@
 package com.ran.kolibri.telegram.bot
 
-import com.ran.kolibri.telegram.bot.app.AppConfig
-import org.springframework.boot.SpringApplication
+import com.ran.kolibri.telegram.bot.starter.TelegramBotStarter
 
-fun main(args: Array<String>) {
-    SpringApplication.run(AppConfig::class.java, *args)
-}
+fun main() = TelegramBotStarter().startApplication()
