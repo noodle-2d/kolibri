3f20f026bf6dfd0c83ee75ad724af8020e07c680
Ilya Kuznetsov <et.anchoret@gmail.com>
2020-03-31 16:20:17 +0300

    TelegramClient was rewritten to user Ktor HttpClient.

---
+++ kolibri-common/src/main/kotlin/com/ran/kolibri/common/telegram/TelegramClient.kt
@@ -0,0 +1,32 @@
+package com.ran.kolibri.common.telegram
+
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.instance
+import com.ran.kolibri.common.dto.config.TelegramClientConfig
+import com.ran.kolibri.common.dto.telegram.SendMessageRequest
+import com.ran.kolibri.common.dto.telegram.SetWebhookRequest
+import com.ran.kolibri.common.dto.telegram.TelegramResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.request.post
+import io.ktor.client.request.url
+
+class TelegramClient(kodein: Kodein) {
+
+    private val httpClient: HttpClient = kodein.instance()
+    private val config: TelegramClientConfig = kodein.instance()
+
+    suspend fun setWebhook(request: SetWebhookRequest): TelegramResponse =
+            httpClient.post {
+                url(buildUrl("/setWebhook"))
+                body = request
+            }
+
+    suspend fun sendMessage(request: SendMessageRequest): TelegramResponse =
+            httpClient.post {
+                url(buildUrl("/sendMessage"))
+                body = request
+            }
+
+    private fun buildUrl(route: String): String =
+            "${config.apiUrl}/bot${config.apiToken}${route}"
+}
--- kolibri-common/src/main/kotlin/com/ran/kolibri/common/telegram/client/TelegramClientConfig.kt
+++
@@ -1,6 +0,0 @@
-package com.ran.kolibri.common.telegram.client
-
-data class TelegramClientConfig(
-        val telegramBotApiUrl: String,
-        val telegramBotToken: String
-)
