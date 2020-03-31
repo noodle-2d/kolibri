0bb71a6868a36e9cb70be337ec7b1f4483e25884
Ilya Kuznetsov <et.anchoret@gmail.com>
2020-03-31 16:23:42 +0300

    TelegramService was rewritten using coroutines.

--- kolibri-telegram-bot/src/main/kotlin/com/ran/kolibri/telegram/bot/service/TelegramService.kt
+++ kolibri-telegram-bot/src/main/kotlin/com/ran/kolibri/telegram/bot/service/TelegramService.kt
@@ -1,42 +1,37 @@
 package com.ran.kolibri.telegram.bot.service
 
-import com.ran.kolibri.common.telegram.client.TelegramClient
-import com.ran.kolibri.common.telegram.client.TelegramClientConfig
-import com.ran.kolibri.common.telegram.dto.SendMessageRequest
-import com.ran.kolibri.common.telegram.dto.SetWebhookRequest
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.instance
+import com.ran.kolibri.common.dto.config.TelegramClientConfig
+import com.ran.kolibri.common.telegram.TelegramClient
+import com.ran.kolibri.common.dto.telegram.SendMessageRequest
+import com.ran.kolibri.common.dto.telegram.SetWebhookRequest
 import com.ran.kolibri.common.util.logInfo
+import com.ran.kolibri.telegram.bot.dto.config.TelegramBotConfig
 import com.ran.kolibri.telegram.bot.dto.updates.UpdatesRequest
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Service
 
-@Service
-class TelegramService {
+class TelegramService(kodein: Kodein) {
 
-    @Autowired
-    private lateinit var telegramClient: TelegramClient
-    @Autowired
-    private lateinit var telegramClientConfig: TelegramClientConfig
+    private val telegramClient: TelegramClient = kodein.instance()
+    private val telegramClientConfig: TelegramClientConfig = kodein.instance()
+    private val telegramBotConfig: TelegramBotConfig = kodein.instance()
 
-    @Value("\${telegram.bot.url}")
-    private lateinit var botUrl: String
-    @Value("\${telegram.bot.owner.id}")
-    private lateinit var ownerId: String
-
-    fun setWebhook() {
-        val webhookUrl = "$botUrl/updates/${telegramClientConfig.telegramBotToken}"
-        logInfo { "Setting webhook. Url: $webhookUrl" }
+    suspend fun setWebhook() {
+        val webhookUrl = "${telegramBotConfig.botUrl}/api/1.x/updates/${telegramClientConfig.apiToken}"
+        logInfo("Setting webhook. Url: $webhookUrl")
 
         val setWebhookRequest = SetWebhookRequest(webhookUrl, listOf("message"))
         val telegramResponse = telegramClient.setWebhook(setWebhookRequest)
-        logInfo { "Webhook was successfully set. Response: $telegramResponse" }
+        logInfo("Webhook was successfully set. Response: $telegramResponse")
     }
 
-    fun processUpdates(request: UpdatesRequest) {
+    suspend fun processUpdates(request: UpdatesRequest) {
+        logInfo("Processing updates request: $request")
+
         val sendMessageRequest = SendMessageRequest(request.message!!.chat!!.id, request.message!!.text)
-        logInfo { "Sending message. Request: $sendMessageRequest" }
+        logInfo("Sending message. Request: $sendMessageRequest")
 
         val telegramResponse = telegramClient.sendMessage(sendMessageRequest)
-        logInfo { "Message was successfully sent. Response: $telegramResponse" }
+        logInfo("Message was successfully sent. Response: $telegramResponse")
     }
 }
