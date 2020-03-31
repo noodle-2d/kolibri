116e6e3c946d8e6de9450ab8f20c75dec40f8b1b
Ilya Kuznetsov <et.anchoret@gmail.com>
2020-03-31 16:27:19 +0300

    Inlining functions was removed from LoggerExtension.

--- kolibri-common/src/main/kotlin/com/ran/kolibri/common/util/LoggerExtension.kt
+++ kolibri-common/src/main/kotlin/com/ran/kolibri/common/util/LoggerExtension.kt
@@ -6,52 +6,52 @@ import org.slf4j.LoggerFactory
 val Any.logger: Logger
     inline get() = LoggerFactory.getLogger(javaClass)
 
-inline fun Any.logTrace(message: String) {
+fun Any.logTrace(message: String) {
     val log = logger
     if (log.isTraceEnabled) log.trace(message)
 }
 
-inline fun Any.logTrace(message: String, throwable: Throwable) {
+fun Any.logTrace(message: String, throwable: Throwable) {
     val log = logger
     if (log.isTraceEnabled) log.trace(message, throwable)
 }
 
-inline fun Any.logDebug(message: String) {
+fun Any.logDebug(message: String) {
     val log = logger
     if (log.isDebugEnabled) log.debug(message)
 }
 
-inline fun Any.logDebug(message: String, throwable: Throwable) {
+fun Any.logDebug(message: String, throwable: Throwable) {
     val log = logger
     if (log.isDebugEnabled) log.debug(message, throwable)
 }
 
-inline fun Any.logInfo(message: String) {
+fun Any.logInfo(message: String) {
     val log = logger
     if (log.isInfoEnabled) log.info(message)
 }
 
-inline fun Any.logInfo(message: String, throwable: Throwable) {
+fun Any.logInfo(message: String, throwable: Throwable) {
     val log = logger
     if (log.isInfoEnabled) log.info(message, throwable)
 }
 
-inline fun Any.logWarn(message: String) {
+fun Any.logWarn(message: String) {
     val log = logger
     if (log.isWarnEnabled) log.warn(message)
 }
 
-inline fun Any.logWarn(message: String, throwable: Throwable) {
+fun Any.logWarn(message: String, throwable: Throwable) {
     val log = logger
     if (log.isWarnEnabled) log.warn(message, throwable)
 }
 
-inline fun Any.logError(message: String) {
+fun Any.logError(message: String) {
     val log = logger
     if (log.isErrorEnabled) log.error(message)
 }
 
-inline fun Any.logError(message: String, throwable: Throwable) {
+fun Any.logError(message: String, throwable: Throwable) {
     val log = logger
     if (log.isErrorEnabled) log.error(message, throwable)
 }
