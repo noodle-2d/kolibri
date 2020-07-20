8994a98eb384d21b56049e3479006e829dadbd7c
ilya-kuznetsov <et.anchoret@gmail.com>
2020-07-20 21:28:42 +0300

    TransactionsValidator object was implemented.

--- kolibri-commandline-utility/src/main/kotlin/com/ran/kolibri/commandline/utility/service/TransactionsValidator.kt
+++ kolibri-commandline-utility/src/main/kotlin/com/ran/kolibri/commandline/utility/service/TransactionsValidator.kt
@@ -3,10 +3,31 @@ package com.ran.kolibri.commandline.utility.service
 import com.ran.kolibri.commandline.utility.dto.import.AccountImportDto
 import com.ran.kolibri.commandline.utility.dto.import.TransactionImportDto
 import com.ran.kolibri.common.util.log
+import java.lang.RuntimeException
+import java.math.BigDecimal
 
 object TransactionsValidator {
 
     fun validate(transactions: List<TransactionImportDto>, accounts: List<AccountImportDto>) {
-        log.info("Validating transactions") // todo
+        log.info("Validating transactions")
+
+        val accountAmountsMap = mutableMapOf<String, BigDecimal>()
+        accounts.forEach { accountAmountsMap[it.importName] = BigDecimal.ZERO }
+
+        transactions.forEach { transaction ->
+            log.info("Validating transaction $transaction")
+            val currentAmount = accountAmountsMap[transaction.accountString]!!
+            val newAmount = currentAmount.plus(transaction.amount)
+            validateAmount(newAmount, transaction)
+            accountAmountsMap[transaction.accountString] = newAmount
+        }
+
+        log.info("Transactions successfully validated")
+    }
+
+    private fun validateAmount(evaluatedAmount: BigDecimal, transaction: TransactionImportDto) {
+        if (evaluatedAmount.compareTo(transaction.resultAmount) != 0) {
+            throw RuntimeException("Amounts mismatch for transaction $transaction, expected - $evaluatedAmount")
+        }
     }
 }
