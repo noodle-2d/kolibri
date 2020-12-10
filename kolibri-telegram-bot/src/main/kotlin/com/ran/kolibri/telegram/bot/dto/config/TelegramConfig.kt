0c76325f8d8902a0d2e9ddbdb99d1c0363004de6
ilya-kuznetsov <et.anchoret@gmail.com>
2020-12-10 13:32:24 +0300

    Telegram bot url config property was removed.

--- kolibri-telegram-bot/src/main/kotlin/com/ran/kolibri/telegram/bot/dto/config/TelegramConfig.kt
+++ kolibri-telegram-bot/src/main/kotlin/com/ran/kolibri/telegram/bot/dto/config/TelegramConfig.kt
@@ -2,16 +2,10 @@ package com.ran.kolibri.telegram.bot.dto.config
 
 import com.typesafe.config.Config
 
-data class TelegramConfig(
-    val botApiUrl: String,
-    val botToken: String,
-    val botUrl: String,
-    val botOwnerId: Int
-) {
+data class TelegramConfig(val botApiUrl: String, val botToken: String, val botOwnerId: Int) {
     constructor(config: Config) : this(
         config.getString("telegram.bot.api-url"),
         config.getString("telegram.bot.token"),
-        config.getString("telegram.bot.url"),
         config.getInt("telegram.bot.owner-id")
     )
 }
