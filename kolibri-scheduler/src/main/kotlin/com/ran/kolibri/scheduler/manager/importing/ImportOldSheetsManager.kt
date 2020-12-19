43d744aa57731b01b6dcd57ca969515e8a493152
ilya-kuznetsov <et.anchoret@gmail.com>
2020-12-19 14:32:22 +0300

    Current year constant was increased.

--- kolibri-scheduler/src/main/kotlin/com/ran/kolibri/scheduler/manager/importing/ImportOldSheetsManager.kt
+++ kolibri-scheduler/src/main/kotlin/com/ran/kolibri/scheduler/manager/importing/ImportOldSheetsManager.kt
@@ -162,7 +162,7 @@ class ImportOldSheetsManager(kodein: Kodein) {
     }
 
     companion object {
-        private val TRANSACTION_RANGE_NAMES = (2015..2020).map { "$it!A2:F" }
+        private val TRANSACTION_RANGE_NAMES = (2015..2021).map { "$it!A2:F" }
         private const val ACCOUNTS_RANGE_NAME = "Счета!A2:B"
         private const val FINANCIAL_ASSETS_RANGE_NAME = "Счета!D22:J"
     }
