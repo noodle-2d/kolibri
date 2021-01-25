f30bd2017cfc5b8bcd67da15ddeb6d99a170c72b
ilya-kuznetsov <et.anchoret@gmail.com>
2021-01-25 22:23:52 +0300

    CurrencyPricesUpdateTask was reverted.

--- kolibri-scheduler/src/main/kotlin/com/ran/kolibri/scheduler/scheduled/task/CurrencyPricesUpdateTask.kt
+++ kolibri-scheduler/src/main/kotlin/com/ran/kolibri/scheduler/scheduled/task/CurrencyPricesUpdateTask.kt
@@ -2,15 +2,14 @@ package com.ran.kolibri.scheduler.scheduled.task
 
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.instance
-import com.ran.kolibri.common.scheduled.task.EveryMinutes
+import com.ran.kolibri.common.scheduled.task.AtFixedHour
 import com.ran.kolibri.common.scheduled.task.Schedule
 import com.ran.kolibri.common.scheduled.task.ScheduledTask
 import com.ran.kolibri.scheduler.manager.prices.CurrencyPricesManager
 
 class CurrencyPricesUpdateTask(kodein: Kodein) : ScheduledTask {
 
-    // private val schedule = AtFixedHour(18) // 21:00 at Moscow time
-    private val schedule = EveryMinutes(1)
+    private val schedule = AtFixedHour(18) // 21:00 at Moscow time
     private val currencyPricesManager: CurrencyPricesManager = kodein.instance()
 
     override fun schedule(): Schedule = schedule
