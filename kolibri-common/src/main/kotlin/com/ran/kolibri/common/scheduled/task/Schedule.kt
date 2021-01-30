651592e6fc0b8bfc2bf799867f6e42af38d5adf8
ilya-kuznetsov <et.anchoret@gmail.com>
2021-01-31 00:30:59 +0300

    Schedule implementations were modified.

--- kolibri-common/src/main/kotlin/com/ran/kolibri/common/scheduled/task/Schedule.kt
+++ kolibri-common/src/main/kotlin/com/ran/kolibri/common/scheduled/task/Schedule.kt
@@ -6,41 +6,22 @@ interface Schedule {
     fun nextTime(): DateTime
 }
 
+class EveryMilliseconds(private val millisecondsInPeriod: Int) : Schedule {
+
+    override fun nextTime(): DateTime =
+        DateTime.now().plusMillis(millisecondsInPeriod)
+}
+
 class EverySeconds(private val secondsInPeriod: Int) : Schedule {
 
-    init {
-        require(secondsInPeriod in 1..59 && 60 % secondsInPeriod == 0)
-    }
-
-    override fun nextTime(): DateTime {
-        val now = DateTime.now()
-        val currentSecond = now.secondOfMinute().get()
-        val periodStart = currentSecond - currentSecond % secondsInPeriod
-
-        return now
-            .withMillisOfSecond(0)
-            .withSecondOfMinute(periodStart)
-            .plusSeconds(secondsInPeriod)
-    }
+    override fun nextTime(): DateTime =
+        DateTime.now().plusSeconds(secondsInPeriod)
 }
 
 class EveryMinutes(private val minutesInPeriod: Int) : Schedule {
 
-    init {
-        require(minutesInPeriod in 1..59 && 60 % minutesInPeriod == 0)
-    }
-
-    override fun nextTime(): DateTime {
-        val now = DateTime.now()
-        val currentMinute = now.minuteOfHour().get()
-        val periodStart = currentMinute - currentMinute % currentMinute
-
-        return now
-            .withMillisOfSecond(0)
-            .withSecondOfMinute(0)
-            .withMinuteOfHour(periodStart)
-            .plusMinutes(minutesInPeriod)
-    }
+    override fun nextTime(): DateTime =
+        DateTime.now().plusMinutes(minutesInPeriod)
 }
 
 class AtFixedHour(private val hour: Int) : Schedule {
--- kolibri-scheduler/src/main/kotlin/com/ran/kolibri/scheduler/scheduled/task/TelegramPullTask.kt
+++ kolibri-scheduler/src/main/kotlin/com/ran/kolibri/scheduler/scheduled/task/TelegramPullTask.kt
@@ -2,14 +2,14 @@ package com.ran.kolibri.scheduler.scheduled.task
 
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.instance
-import com.ran.kolibri.common.scheduled.task.EverySeconds
+import com.ran.kolibri.common.scheduled.task.EveryMilliseconds
 import com.ran.kolibri.common.scheduled.task.Schedule
 import com.ran.kolibri.common.scheduled.task.ScheduledTask
 import com.ran.kolibri.scheduler.manager.TelegramUpdatesManager
 
 class TelegramPullTask(kodein: Kodein) : ScheduledTask {
 
-    private val schedule = EverySeconds(2)
+    private val schedule = EveryMilliseconds(1500)
     private val telegramUpdatesManager: TelegramUpdatesManager = kodein.instance()
 
     override fun schedule(): Schedule = schedule
