2fda036b3b058f97febe7b6d4a9bb1bb462e5a75
ilya-kuznetsov <et.anchoret@gmail.com>
2021-01-30 17:38:17 +0300

    TelegramManager was supplemented by new ChatContact mechanism.

--- kolibri-common/src/main/kotlin/com/ran/kolibri/common/manager/TelegramManager.kt
+++ kolibri-common/src/main/kotlin/com/ran/kolibri/common/manager/TelegramManager.kt
@@ -3,19 +3,70 @@ package com.ran.kolibri.common.manager
 import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.instance
 import com.ran.kolibri.common.client.telegram.TelegramClient
-import com.ran.kolibri.common.client.telegram.model.SendMessageRequest
+import com.ran.kolibri.common.client.telegram.model.Button
+import com.ran.kolibri.common.client.telegram.model.ReplyMarkup
+import com.ran.kolibri.common.client.telegram.model.SendButtonMessageRequest
+import com.ran.kolibri.common.client.telegram.model.SendTextMessageRequest
 import com.ran.kolibri.common.client.telegram.model.TelegramConfig
 import com.ran.kolibri.common.util.log
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.withContext
+import org.joda.time.DateTime
 
 class TelegramManager(kodein: Kodein) {
 
     private val telegramClient: TelegramClient = kodein.instance()
     private val telegramConfig: TelegramConfig = kodein.instance()
 
-    suspend fun sendMessageToOwner(text: String) {
-        log.info("Sending message to owner: $text")
-        val sendMessageRequest = SendMessageRequest(telegramConfig.botOwnerId, text)
-        val telegramResponse = telegramClient.sendMessage(sendMessageRequest)
-        log.info("Message was successfully sent. Response: $telegramResponse")
+    suspend fun sendMessageToOwner(text: String, options: List<ListOption> = listOf()): Int? {
+        log.info("Sending message to owner: $text [$options]")
+
+        val sendMessageRequest = when (val replyMarkup = buildReplyMarkup(options)) {
+            null -> SendTextMessageRequest(telegramConfig.botOwnerId, text)
+            else -> SendButtonMessageRequest(telegramConfig.botOwnerId, text, replyMarkup)
+        }
+        val sendMessageResponse = telegramClient.sendMessage(sendMessageRequest)
+
+        log.info("Message was successfully sent. Response: $sendMessageResponse")
+        return sendMessageResponse.result?.messageId
+    }
+
+    private fun buildReplyMarkup(options: List<ListOption>): ReplyMarkup? =
+        options
+            .map { listOf(Button(it.name, it.id)) }
+            .let { if (it.isEmpty()) null else it }
+            ?.let { ReplyMarkup(it) }
+
+    suspend fun doActionSettingChatContext(action: suspend () -> ChatContext) =
+        doActionUpdatingChatContext { Processed(action()) }
+
+    suspend fun doActionUpdatingChatContext(action: suspend (ChatContext?) -> ProcessingInContextResult) =
+        withContext(threadContext) {
+            if (lastChatContextUpdateTime.plusHours(1).isBeforeNow) {
+                chatContext = null
+            }
+
+            when (val processingResult = action(chatContext)) {
+                is Processed -> chatContext = processingResult.newChatContext
+            }
+            log.debug("Now chat context is $chatContext")
+        }
+
+    companion object {
+        private val threadContext = newSingleThreadContext("ChatContext")
+        private var lastChatContextUpdateTime = DateTime()
+        private var chatContext: ChatContext? = null
     }
 }
+
+interface ChatContext {
+    val messageId: Int
+}
+
+interface ProcessingInContextResult
+
+object Ignored : ProcessingInContextResult
+
+data class Processed(val newChatContext: ChatContext?): ProcessingInContextResult
+
+data class ListOption(val name: String, val id: String)
