d44b8ac9b98951e318a9503acecd92e8adf1b3f4
ilya-kuznetsov <et.anchoret@gmail.com>
2021-01-31 14:06:24 +0300

    TelegramManager was supplemented by edit message method.

--- kolibri-common/src/main/kotlin/com/ran/kolibri/common/manager/TelegramManager.kt
+++ kolibri-common/src/main/kotlin/com/ran/kolibri/common/manager/TelegramManager.kt
@@ -4,6 +4,8 @@ import com.github.salomonbrys.kodein.Kodein
 import com.github.salomonbrys.kodein.instance
 import com.ran.kolibri.common.client.telegram.TelegramClient
 import com.ran.kolibri.common.client.telegram.model.Button
+import com.ran.kolibri.common.client.telegram.model.EditButtonMessageRequest
+import com.ran.kolibri.common.client.telegram.model.EditTextMessageRequest
 import com.ran.kolibri.common.client.telegram.model.ReplyMarkup
 import com.ran.kolibri.common.client.telegram.model.SendButtonMessageRequest
 import com.ran.kolibri.common.client.telegram.model.SendTextMessageRequest
@@ -18,7 +20,7 @@ class TelegramManager(kodein: Kodein) {
     private val telegramClient: TelegramClient = kodein.instance()
     private val telegramConfig: TelegramConfig = kodein.instance()
 
-    suspend fun sendMessageToOwner(text: String, options: List<ListOption> = listOf()): Int? {
+    suspend fun sendMessageToOwner(text: String, options: List<ListOption> = listOf()): Int {
         log.info("Sending message to owner: $text [$options]")
 
         val sendMessageRequest = when (val replyMarkup = buildReplyMarkup(options)) {
@@ -28,7 +30,20 @@ class TelegramManager(kodein: Kodein) {
         val sendMessageResponse = telegramClient.sendMessage(sendMessageRequest)
 
         log.info("Message was successfully sent. Response: $sendMessageResponse")
-        return sendMessageResponse.result?.messageId
+        return sendMessageResponse.result!!.messageId
+    }
+
+    suspend fun editMessageToOwner(messageId: Int, text: String, options: List<ListOption> = listOf()): Int {
+        log.info("Edit message $messageId to owner: $text [$options]")
+
+        val editMessageRequest = when (val replyMarkup = buildReplyMarkup(options)) {
+            null -> EditTextMessageRequest(telegramConfig.botOwnerId, messageId, text)
+            else -> EditButtonMessageRequest(telegramConfig.botOwnerId, messageId, text, replyMarkup)
+        }
+        val editMessageResponse = telegramClient.editMessage(editMessageRequest)
+
+        log.info("Message was successfully edited. Response: $editMessageResponse")
+        return editMessageResponse.result!!.messageId
     }
 
     private fun buildReplyMarkup(options: List<ListOption>): ReplyMarkup? =
