e0aa875886341585b04042e19cb33d6727d8e09b
ilya-kuznetsov <et.anchoret@gmail.com>
2021-05-04 00:35:15 +0300

    ExposedExtension.kt file was supplemented by json columns support.

--- kolibri-common/src/main/kotlin/com/ran/kolibri/common/util/ExposedExtension.kt
+++ kolibri-common/src/main/kotlin/com/ran/kolibri/common/util/ExposedExtension.kt
@@ -1,9 +1,15 @@
 package com.ran.kolibri.common.util
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.joda.JodaModule
 import java.math.BigDecimal
 import kotlin.reflect.KClass
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.TextColumnType
+import org.jetbrains.exposed.sql.statements.api.PreparedStatementApi
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.postgresql.util.PGobject
 
@@ -32,3 +38,27 @@ fun Table.decimal(name: String): Column<BigDecimal> =
 
 private const val DEFAULT_DECIMAL_PRECISION = 22
 private const val DEFAULT_DECIMAL_SCALE = 4
+
+inline fun <reified T : Any> Table.json(name: String): Column<T> =
+    registerColumn(name, JsonColumnType(T::class.java))
+
+class JsonColumnType<T : Any>(private val klass: Class<T>) : TextColumnType() {
+
+    override fun valueFromDB(value: Any): T {
+        val stringValue = super.valueFromDB(value).toString()
+        return JSON_MAPPER.readValue(stringValue, klass)
+    }
+
+    override fun setParameter(stmt: PreparedStatementApi, index: Int, value: Any?) {
+        val stringValue = value?.let { JSON_MAPPER.writeValueAsString(it) }
+        super.setParameter(stmt, index, stringValue)
+    }
+
+    companion object {
+        private val JSON_MAPPER = ObjectMapper().apply {
+            registerModule(JodaModule())
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        }
+    }
+}
