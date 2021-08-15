a3314e2f6919c11855df1b6f728895e356da79c6
ilya-kuznetsov <et.anchoret@gmail.com>
2021-08-16 01:03:18 +0300

    KafkaConfig was supplemented by processor configs.

--- kolibri-common/src/main/kotlin/com/ran/kolibri/common/kafka/KafkaConfig.kt
+++ kolibri-common/src/main/kotlin/com/ran/kolibri/common/kafka/KafkaConfig.kt
@@ -3,18 +3,31 @@ package com.ran.kolibri.common.kafka
 import com.ran.kolibri.common.util.getConfigMap
 import com.typesafe.config.Config
 
-// todo: add consumers config properties here
-data class KafkaConfig(val bootstrapServers: String, val producers: Map<String, KafkaProducerConfig>) {
+data class KafkaConfig(
+    val bootstrapServers: String,
+    val producers: Map<String, KafkaProducerConfig>,
+    val processors: Map<String, KafkaProcessorConfig>
+) {
     constructor(config: Config) : this(
         config.getString("kafka.bootstrap-servers"),
         config
             .getConfigMap("kafka.producers")
-            .mapValues { KafkaProducerConfig(it.value) }
+            .mapValues { KafkaProducerConfig(it.value) },
+        config
+            .getConfigMap("kafka.processors")
+            .mapValues { KafkaProcessorConfig(it.value) }
     )
 }
 
 data class KafkaProducerConfig(val topic: String) {
     constructor(config: Config) : this(
         config.getString("topic")
+    )
+}
+
+data class KafkaProcessorConfig(val topic: String, val consumerGroup: String) {
+    constructor(config: Config) : this(
+        config.getString("topic"),
+        config.getString("consumer-group")
     )
 }
